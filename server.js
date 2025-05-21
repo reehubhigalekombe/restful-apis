782717f0c1dfdac567ae24df2d37698868289e58
Higal Ekombe <higalekombe@gmail.com>
2025-05-21 23:46:16 +0300

    created a RESTful APIs with http methods
    created a RESTful APIs with http methods

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5003;
+
+app.use(express.json());
+
+let users = [
+    {id: 1, name: "Higal"},
+    {id: 2, name: "Pauline"},
+]
+
+app.get("/users", (req, res) => {
+    res.json(users)
+});
+
+app.get("/users", (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if(!user) return res.status(404).send("user cannot be found")
+});
+
+app.post("users", (req, res) => {
+    const newUser = {
+        id: users.length+1,
+        name: req.body.name
+    };
+    users.push(newUser);
+    res.status(201).json(newUsers)
+});
+
+app.put("users/:id", (reg, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if(!user) return res.status(404).send("user not found");
+    user.name = req.body.name
+});
+
+app.delete("/users/:id", (req, res) => {
+    users = users.filter(u => u.id !== parseInt(req.params.id));
+    res.send("user deleted")
+});
+app.get("/", (req, res) => {
+    res.send({message: "Hello Higal"})
+});
+app.listen(PORT, () => {
+    console.log(`Server is connected ${PORT}`)
+}
+)
